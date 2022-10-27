73e4a5be3b7a981b3eed63cd4f29ad93f016a950
Tiffany Huang <tiffhuang78@gmail.com>
2022-10-27 16:19:39 -0400

    restructured code for running experiments

--- SMD_opt.py
+++ SMD_opt.py
@@ -8,15 +8,17 @@ import math
 
 class SMD_compress(Optimizer):
 
-    def __init__(self, params, lr=0.01, momentum=0, weight_decay = 0, dampening=0):
+    def __init__(self, params, lr=0.01, momentum=0, weight_decay = 0, dampening=0, eps=0.1):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= momentum:
             raise ValueError("Invalid momentum value: {}".format(momentum))
         if not 0.0 <= weight_decay:
             raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+        if not 0.0 < eps:
+            raise ValueError("Invalid eps value: {}".format(eps))
 
-        defaults = dict(lr=lr, momentum=momentum, weight_decay=weight_decay, dampening=dampening)
+        defaults = dict(lr=lr, momentum=momentum, weight_decay=weight_decay, dampening=dampening, eps=eps)
         super(SMD_compress, self).__init__(params, defaults)
     
     def __setstate__(self, state):
@@ -48,9 +50,8 @@ class SMD_compress(Optimizer):
                         buf.mul_(momentum).add_(1 - dampening, d_p)
                     d_p = buf
     #           (1+eps) norm potential function
-                eps = 0.1
-                update = (1+eps)* (torch.abs(p.data)**eps) * torch.sign(p.data) - group['lr'] * d_p
-                p.data = (torch.abs(update/(1+eps))**(1/eps)) * torch.sign(update)
+                update = (1+group['eps'])* (torch.abs(p.data)**group['eps']) * torch.sign(p.data) - group['lr'] * d_p
+                p.data = (torch.abs(update/(1+group['eps']))**(1/group['eps'])) * torch.sign(update)
 
         return loss 
     
--- qnorm_train.py
+++ qnorm_train.py
@@ -1,252 +1,224 @@
+from argparse import ArgumentParser
+import time 
+import numpy as np
+import yaml
+from tqdm import tqdm 
+import sys, os, os.path
+from copy import deepcopy
+
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import math
 import torchvision
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 import torchvision.models as models
+
 import SMD_opt
+from models import *
 
-class BasicBlock(nn.Module):
-    expansion = 1
+from fastargs import get_current_config
+from fastargs.decorators import param
+from fastargs import Param, Section
 
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+Section('training', 'Hyperparameters').params(
+    arch=Param(str, 'CNN architecture to use', required=True),
+    pnorm=Param(float, 'p-value to use in SMD', required=True),
+    # lr_init=Param(float, 'The initial learning rate to use', required=True),
+    lr=Param(float, 'The maximum learning rate to use', required=True),
+    epochs=Param(int, 'Number of epochs to run for', required=True),
+    # lr_peak_epoch=Param(int, 'Peak epoch for cyclic lr', required=True),
+    batch_size=Param(int, 'Batch size', default=128),
+    num_workers=Param(int, 'The number of workers', default=8),
+)
 
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-
-def ResNet18():
-    return ResNet(BasicBlock, [2,2,2,2])
-
-def ResNet34():
-    return ResNet(BasicBlock, [3,4,6,3])
-
-def ResNet50():
-    return ResNet(Bottleneck, [3,4,6,3])
-
-
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-
-import torchvision
-import torchvision.transforms as transforms
-
-
-
-
-num_epochs = 1000
-num_classes = 10
-batch_size = 128
-learning_rate = 1e-9
-q_value = 10
+Section('data', 'arguments for loading data').params(
+    output_directory=Param(str, 'directory to save outputs', required=True),
+    raw_data=Param(str, 'Where the raw data can be found', required=True)
+)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('device:', device)
 
 # Data
-print('==> Preparing data..')
+@param('data.raw_data')
+@param('training.batch_size')
+@param('training.num_workers')
+def make_dataloaders(raw_data=None, batch_size=None, num_workers=None):
+    print('==> Preparing data..')
 
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=2),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
-])
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=2),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+    ])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
-])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+    ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4)
+    raw_data = os.path.expandvars(raw_data)
+    trainset = torchvision.datasets.CIFAR10(root=raw_data, train=True, download=False, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root=raw_data, train=False, download=False, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, testloader, classes
 
 # Model
-print('==> Building model..')
+@param('training.arch')
+def construct_model(arch):
+    print('==> Building model..')
 
-model = ResNet18()
+    model = None
+    if "arch" == 'resnet':
+        model = ResNet18()
+    else:
+        model = MobileNetV2()
 
-if device == 'cuda':
-    model = torch.nn.DataParallel(model)
-    cudnn.benchmark = True
-model = model.cuda()
+    if device == 'cuda':
+        model = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+    model = model.cuda()
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.01)
-    elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(0.0, 0.01)
-        m.bias.data.fill_(0)
-    elif classname.find('Linear') != -1:
-        m.weight.data.uniform_(-0.01, 0.01)
-        m.bias.data.uniform_(-0.1, 0.1)
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            m.weight.data.normal_(0.0, 0.01)
+        elif classname.find('BatchNorm') != -1:
+            m.weight.data.normal_(0.0, 0.01)
+            m.bias.data.fill_(0)
+        elif classname.find('Linear') != -1:
+            m.weight.data.uniform_(-0.01, 0.01)
+            m.bias.data.uniform_(-0.1, 0.1)
+        
+    # model.apply(weights_init)
+
+    free_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('free_params:', free_params)
+
+    w = torch.nn.utils.parameters_to_vector(model.parameters())
+    print(w)
+    return model
+
+@param('training.lr')
+@param('training.epochs')
+@param('training.pnorm')
+def train(model, trainloader, testloader, log_file=sys.stdout, output_directory='./', lr=None, epochs=None, pnorm=None):
+    criterion = nn.CrossEntropyLoss().cuda()
+    if pnorm < 2.0:
+        optimizer = SMD_opt.SMD_compress(model.parameters(), lr=lr, eps=pnorm-1)
+    else:
+        optimizer = SMD_opt.SMD_qnorm(model.parameters(), lr=lr, q=pnorm)
+
+    total_step = len(trainloader)
+    print('total_step:', total_step)
+
+    best_test_acc = 0
+
+    # Training
+    for epoch in tqdm(range(epochs)):
+        model.train()
+
+        for i, (images, labels) in enumerate(trainloader):
+            
+            images = images.cuda(non_blocking=True)
+            labels = labels.cuda(non_blocking=True)
+            
+            images = Variable(images)
+            labels = Variable(labels)
+            # Run the forward pass
+            outputs = model(images)
+            
+            loss = criterion(outputs, labels)
+
+            # Backprop and perform
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+       
+        w = torch.nn.utils.parameters_to_vector(model.parameters())
+        print(w)
+        if (epoch + 1) % 10 == 0:
+            print(f'Epoch {epoch+1} loss: {loss.item():.4f}', file = log_file)
+            train_acc, test_acc = evaluate(model, trainloader, testloader)
+            print(f'Epoch {epoch+1} train acc: {train_acc:.4f}', file = log_file)
+            print(f'Epoch {epoch+1} test acc: {test_acc:.4f}', file = log_file)
+            log_file.flush()
+
+            if test_acc > best_test_acc:
+                print("Saving best model...", file=log_file)
+                best_test_acc = test_acc
+                torch.save(model, f'{output_directory}/best_model.pt')
+
+            if train_acc == 1.0 :
+                break
+
+    print('Finished Training')
+
+def evaluate(model, trainloader, testloader):
+    # Test the model
+    model.eval()
+    acc = []
+    with torch.no_grad():
+        for loader in (trainloader, testloader):
+            correct = 0
+            total = 0
+            for images, labels in loader:
+                images = images.cuda(non_blocking=True)
+                labels = labels.cuda(non_blocking=True)
+                
+                images = Variable(images)
+                labels = Variable(labels)
+                
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+            print(correct, total)
+            acc.append(correct / total)
+
+    return acc[0], acc[1]
+
+
+if __name__ == "__main__":
+    config = get_current_config()
+    parser = ArgumentParser(description='CIFAR-10 training')
+    config.augment_argparse(parser)
+
+    # loads from args.config_path if provided
+    config.collect_argparse_args(parser)
+    config.validate(mode='stderr')
+    config.summary()
+
+    print(config)
+
+    output_dir = os.path.expandvars(config['data.output_directory'])
+    os.makedirs(output_dir, exist_ok=True)
+
+    start_time = time.time()
+    trainloader, testloader, _ = make_dataloaders()
+    print(f'Data Load time (min): {(time.time() - start_time) / 60:.2f}')
     
-model.apply(weights_init)
+    model = construct_model()
+    with open(f'{output_dir}/log.txt', 'w') as log_file:
+        train(model, trainloader, testloader, log_file, output_dir)
 
-free_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(free_params)
+    train_acc, test_acc = evaluate(model, trainloader, testloader)
+    accuracies = {'train': train_acc, 'test': test_acc}
+    print(f'train accuracy: {train_acc*100:.2f}%')
+    print(f'test accuracy: {test_acc*100:.2f}%')
+    acc_file = f'{output_dir}/accuracy.yaml'
+    with open(acc_file, 'w') as file:
+        yaml.dump(accuracies, file)
 
+    torch.save(model.state_dict(), f'{output_dir}/final_model.pt') 
 
-criterion = nn.CrossEntropyLoss().cuda()
-optimizer = SMD_opt.SMD_qnorm(model.parameters(), lr=learning_rate, q=q_value)
-
-total_step = len(trainloader)
-print(total_step)
-loss_list = []
-acc_list = []
-
-
-# Training
-for epoch in range(num_epochs):
-    total = 0
-    correct = 0
-#     scheduler.step()
-    for i, (images, labels) in enumerate(trainloader):
-        
-        images = images.cuda(async=True)
-        labels = labels.cuda(async=True)
-        
-        images = Variable(images)
-        labels = Variable(labels)
-        # Run the forward pass
-        outputs = model(images)
-        
-        loss = criterion(outputs, labels)
-        loss_list.append(loss.item())
-
-        # Backprop and perform
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-         # Track the accuracy
-        total = labels.size(0) + total
-        _, predicted = torch.max(outputs.data, 1)
-        correct = (predicted == labels).sum().item() + correct
-        acc_list.append(correct / total)
-        
-        if (i + 1) % 100 == 0:
-            w = torch.nn.utils.parameters_to_vector(model.parameters())
-            print(w)
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
-                          (correct / total) * 100))
-    if (total == correct) :
-        break 
-
-print('Finished Training')
-
-
-
-
-torch.save(model.state_dict(), './final_2norm.pth') 
-
-
-# Test the model
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in testloader:
-        images = images.cuda(async=True)
-        labels = labels.cuda(async=True)
-        
-        images = Variable(images)
-        labels = Variable(labels)
-        
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
+    print(f'Total time (min): {(time.time() - start_time) / 60:.2f}')
