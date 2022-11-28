38c381c349ff35df13e19b5d43c6208e43048a30
Tiffany Huang <tiffhuang78@gmail.com>
2022-11-28 04:09:28 -0500

    match original code

--- qnorm_train.py
+++ qnorm_train.py
@@ -1,10 +1,12 @@
 from argparse import ArgumentParser
 import time 
-import numpy as np
 import yaml
 from tqdm import tqdm 
 import sys, os, os.path
 from copy import deepcopy
+
+import random 
+import numpy as np 
 
 import torch
 import torch.optim as optim
@@ -38,6 +40,11 @@ Section('data', 'arguments for loading data').params(
     raw_data=Param(str, 'Where the raw data can be found', required=True)
 )
 
+Section('checkpoint', 'for when training from a checkpoint').params(
+    from_checkpoint=Param(bool, 'whether to train from a checkpoint', required=True),
+    trial=Param(int, 'which trial number to grab checkpoint from', required=True)
+)
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('device:', device)
 
@@ -61,11 +68,12 @@ def make_dataloaders(raw_data=None, batch_size=None, num_workers=None):
     ])
 
     raw_data = os.path.expandvars(raw_data)
-    trainset = torchvision.datasets.CIFAR10(root=raw_data, train=True, download=False, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    print('data from:', raw_data)
+    trainset = torchvision.datasets.CIFAR10(root=raw_data, train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4)
 
-    testset = torchvision.datasets.CIFAR10(root=raw_data, train=False, download=False, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    testset = torchvision.datasets.CIFAR10(root=raw_data, train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -77,13 +85,19 @@ def construct_model(arch):
     print('==> Building model..')
 
     model = None
-    if "arch" == 'resnet':
+    if arch == 'resnet':
         model = ResNet18()
-    else:
+    elif arch == 'vgg':
+        model = VGG('VGG11')
+    elif arch == 'mobilenet':
         model = MobileNetV2()
+    elif arch == 'efficientnet':
+        model = EfficientNetB0()
+    elif arch == 'regnet':
+        model = RegNetX_200MF()
 
     if device == 'cuda':
-        model = torch.nn.DataParallel(model)
+        # model = torch.nn.DataParallel(model)
         cudnn.benchmark = True
     model = model.cuda()
 
@@ -98,19 +112,21 @@ def construct_model(arch):
             m.weight.data.uniform_(-0.01, 0.01)
             m.bias.data.uniform_(-0.1, 0.1)
         
-    # model.apply(weights_init)
+    model.apply(weights_init)
 
     free_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('free_params:', free_params)
 
-    w = torch.nn.utils.parameters_to_vector(model.parameters())
-    print(w)
+    # w = torch.nn.utils.parameters_to_vector(model.parameters())
+    # print(w)
     return model
 
 @param('training.lr')
 @param('training.epochs')
 @param('training.pnorm')
-def train(model, trainloader, testloader, log_file=sys.stdout, output_directory='./', lr=None, epochs=None, pnorm=None):
+@param('checkpoint.from_checkpoint')
+@param('checkpoint.trial')
+def train(model, trainloader, testloader, log_file=sys.stdout, output_directory='./', lr=None, epochs=None, pnorm=None, from_checkpoint=False, trial=0):
     criterion = nn.CrossEntropyLoss().cuda()
     if pnorm < 2.0:
         optimizer = SMD_opt.SMD_compress(model.parameters(), lr=lr, eps=pnorm-1)
@@ -119,12 +135,27 @@ def train(model, trainloader, testloader, log_file=sys.stdout, output_directory=
 
     total_step = len(trainloader)
     print('total_step:', total_step)
+    print("learning rate =", lr)
+    print("pnorm =", pnorm)
 
     best_test_acc = 0
 
+    start_epoch = 0
+    if from_checkpoint:
+        checkpoint = torch.load(f'{output_directory}/checkpoint.pt')
+        model.load_state_dict(checkpoint['model_state_dict'])
+        opt.load_state_dict(checkpoint['optimizer_state_dict'])
+        scaler.load_state_dict(checkpoint['scaler_state_dict'])
+        start_epoch = checkpoint['epoch']
+        best_test_acc = checkpoint['best_test_acc']
+        print(f"Training from epoch {start_epoch+1}; current best test acc = {best_test_acc}", file=log_file)
+
     # Training
-    for epoch in tqdm(range(epochs)):
+    for epoch in tqdm(range(start_epoch, epochs)):
         model.train()
+
+        total = 0
+        correct = 0
 
         for i, (images, labels) in enumerate(trainloader):
             
@@ -142,50 +173,57 @@ def train(model, trainloader, testloader, log_file=sys.stdout, output_directory=
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+            # Track the accuracy
+            total = labels.size(0) + total
+            _, predicted = torch.max(outputs.data, 1)
+            correct = (predicted == labels).sum().item() + correct
+
+            if (i + 1) % 100 == 0:
+                w = torch.nn.utils.parameters_to_vector(model.parameters())
+                print(w)
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+                    .format(epoch + 1, epochs, i + 1, total_step, loss.item(),
+                            (correct / total) * 100))
        
-        w = torch.nn.utils.parameters_to_vector(model.parameters())
-        print(w)
-        if (epoch + 1) % 10 == 0:
-            print(f'Epoch {epoch+1} loss: {loss.item():.4f}', file = log_file)
-            train_acc, test_acc = evaluate(model, trainloader, testloader)
-            print(f'Epoch {epoch+1} train acc: {train_acc:.4f}', file = log_file)
-            print(f'Epoch {epoch+1} test acc: {test_acc:.4f}', file = log_file)
-            log_file.flush()
+        print('epoch {}: {} correct out of {}, loss: {}'.format(epoch+1, correct, total, loss.item()), file=log_file)
+        if (total == correct) :
+            break
 
-            if test_acc > best_test_acc:
-                print("Saving best model...", file=log_file)
-                best_test_acc = test_acc
-                torch.save(model, f'{output_directory}/best_model.pt')
-
-            if train_acc == 1.0 :
-                break
+        if (epoch + 1) % 25 == 0:
+            print("Checkpointing...", file=log_file)
+            print(f"{total_correct} correct out of {total_num}", file=log_file)
+            torch.save({
+                'epoch': epoch + 1,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': opt.state_dict(),
+                'scaler_state_dict': scaler.state_dict(),
+                'best_test_acc': best_test_acc,
+            }, f'{output_directory}/checkpoint.pt')
 
     print('Finished Training')
 
-def evaluate(model, trainloader, testloader):
+def evaluate(model, loader):
     # Test the model
     model.eval()
-    acc = []
     with torch.no_grad():
-        for loader in (trainloader, testloader):
-            correct = 0
-            total = 0
-            for images, labels in loader:
-                images = images.cuda(non_blocking=True)
-                labels = labels.cuda(non_blocking=True)
-                
-                images = Variable(images)
-                labels = Variable(labels)
-                
-                outputs = model(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+        correct = 0
+        total = 0
+        for images, labels in loader:
+            images = images.cuda(non_blocking=True)
+            labels = labels.cuda(non_blocking=True)
+            
+            images = Variable(images)
+            labels = Variable(labels)
+            
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-            print(correct, total)
-            acc.append(correct / total)
+        print(correct, total)
 
-    return acc[0], acc[1]
+    return correct / total
 
 
 if __name__ == "__main__":
@@ -198,22 +236,34 @@ if __name__ == "__main__":
     config.validate(mode='stderr')
     config.summary()
 
-    print(config)
+    if config['checkpoint.from_checkpoint']:
+        print("checkpointing!")
+        os.environ["OUTPUT"] = "$GROUP/smd-experiment/cifar10_StochasticMirrorDescent-long-run/" + str(config['checkpoint.trial'])
 
     output_dir = os.path.expandvars(config['data.output_directory'])
-    os.makedirs(output_dir, exist_ok=True)
+    print(output_dir)
+    output_dir = os.path.expandvars(output_dir)
+    print(output_dir)
+
+    if not config['checkpoint.from_checkpoint']:
+        print("new training!")
+        os.makedirs(output_dir, exist_ok = True)
 
     start_time = time.time()
     trainloader, testloader, _ = make_dataloaders()
     print(f'Data Load time (min): {(time.time() - start_time) / 60:.2f}')
     
     model = construct_model()
-    with open(f'{output_dir}/log.txt', 'w') as log_file:
+    # save initial model so we can view weights later
+    if not config['checkpoint.from_checkpoint']:
+        torch.save(model, f'{output_dir}/init_model.pt')
+
+    with open(f'{output_dir}/log.txt', 'a') as log_file:
+        log_file.write('\n')
         train(model, trainloader, testloader, log_file, output_dir)
 
-    train_acc, test_acc = evaluate(model, trainloader, testloader)
-    accuracies = {'train': train_acc, 'test': test_acc}
-    print(f'train accuracy: {train_acc*100:.2f}%')
+    test_acc = evaluate(model, testloader)
+    accuracies = {'test': test_acc}
     print(f'test accuracy: {test_acc*100:.2f}%')
     acc_file = f'{output_dir}/accuracy.yaml'
     with open(acc_file, 'w') as file:
